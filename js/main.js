1e046a5ff37c2c99ceee0d3f85ba8e329582490e
Domenic Garreffa <garreffd@uwindsor.ca>
2017-11-27 13:20:28 -0500

    Working version. Need to press conn button to connect to scoreboard

--- js/main.js
+++ js/main.js
@@ -1,44 +1,56 @@
-
-window.onload = function () {
-	
-	// TODO:: Do your initialization job
-	var redScore = 0;
-	var blueScore = 0;
+var redScore = 0;
+var blueScore = 0;
+//References to Bluetooth LE adapter and the connected Scoreboard.
+var adapter;
+var remoteDevice = null;
+//References to the Scoreboard manager service and included characteristics.
+var serviceUUIDs;
+var gattService;
+var red_score_characteristic;
+var blue_score_characteristic;
+//Gets written to characteristic.
+var redScoreArray = [0];
+var blueScoreArray = [0];
+    
+window.onload = function () {	
     // add eventListener for tizenhwkey
     document.addEventListener('tizenhwkey', function(e) {
-        if(e.keyName == "back")
-	try {
-	    tizen.application.getCurrentApplication().exit();
-	} catch (ignore) {
-	}
+        if(e.keyName === "back")
+	        {
+			try {
+			    tizen.application.getCurrentApplication().hide();
+			} catch (ignore) {
+				
+			}
+        }
     });
-
-    // Sample code
+    
+   
+    adapter = tizen.bluetooth.getLEAdapter();
+ 
+    
     var redScoreHalf = document.getElementById("red_half_court");
     var blueScoreHalf = document.getElementById("blue_half_court");
     var redScoreText = document.getElementById("red_score");
     var blueScoreText = document.getElementById("blue_score");
+    
     var minusRedScoreButton = document.getElementById("minus_red_score_button");
     var minusBlueScoreButton = document.getElementById("minus_blue_score_button");
-    var redScoreArray = [0];
-    var blueScoreArray = [0];
-    
+    var resetButton = document.getElementById("new_game_button");
+    var connButton = document.getElementById("conn_button"); 
+   
     redScoreHalf.addEventListener("click", function(){
-    	console.log("Check");
     	redScore++;
     	if(redScore < 0){
     		redScore = 0;
     	}
-    	
-    	if(!remoteDevice.isConnected){
-  		  remoteDevice.connect(connectSuccess, connectFail);
-    	}else{
     	redScoreArray[0] = redScore;
-    	red_score_characteristic.writeValue( redScoreArray  ,writeSuccess, writeFail);
+    	
+    	if(remoteDevice !== null && remoteDevice.isConnected){
+    	red_score_characteristic.writeValue( redScoreArray, writeSuccess, writeFail);
     	}
-    	redScoreText.innerHTML = ""  + redScore;
     	
-    	
+    	render();
     });
     
     
@@ -50,12 +62,13 @@ window.onload = function () {
     	
     	blueScoreArray[0] = blueScore;
     	
-    	
-    	blue_score_characteristic.writeValue( blueScoreArray  ,writeSuccess, writeFail);
-    	blueScoreText.innerHTML = ""  + blueScore;
+    	if(remoteDevice !== null && remoteDevice.isConnected){
+    	blue_score_characteristic.writeValue( blueScoreArray, writeSuccess, writeFail);
+    	}
+    	render();
     });
     
-    var resetButton = document.getElementById("new_game_button");
+  
     
     resetButton.addEventListener("click", function(){
     	var isNewGame = confirm("Are you sure?");
@@ -67,20 +80,33 @@ window.onload = function () {
     	redScore = 0;
     	blueScore = 0;
     	
-    	redScoreText.innerHTML = ""  + redScore;
-    	blueScoreText.innerHTML = ""  + blueScore;
+    	blueScoreArray[0] = blueScore;
+    	redScoreArray[0] = redScore;
+    	
+    	if(remoteDevice !== null && remoteDevice.isConnected){
+    		red_score_characteristic.writeValue( redScoreArray, writeSuccess, writeFail);
+    		
+    		setTimeout(function(){ 		
+        		blue_score_characteristic.writeValue( blueScoreArray, writeSuccess, writeFail);
+    		}, 500);
+    	}
+    	render();
     });
-    
-    
-    
-   
     
     minusBlueScoreButton.addEventListener("click",function(){
     	blueScore--;
     	if(blueScore < 0){
     		blueScore = 0;
     	}
-    	blueScoreText.innerHTML = ""  + blueScore;
+    	
+    	blueScoreArray[0] = blueScore;
+    	
+    	if(remoteDevice !== null && remoteDevice.isConnected){
+        	blue_score_characteristic.writeValue( blueScoreArray, writeSuccess, writeFail);
+        	}
+    	
+    	render();
+    	
     });
     
     minusRedScoreButton.addEventListener("click",function(){
@@ -88,29 +114,21 @@ window.onload = function () {
     	if(redScore < 0){
     		redScore = 0;
     	}
-    	redScoreText.innerHTML = ""  + redScore;
+    	
+    	redScoreArray[0] = redScore;
+    	
+      	if(remoteDevice !== null && remoteDevice.isConnected){
+        	red_score_characteristic.writeValue( redScoreArray, writeSuccess, writeFail);
+        	}
+    	render();
     });
 
-   
-    var adapter = tizen.bluetooth.getLEAdapter();
-    //var serviceUUID = 0x0113233445566778899AABBCCDDEEFF1;
-    /* Retrieve known devices */
-   
-    
-    var red_score_characteristic;
-    var blue_score_characteristic;
-    
-    var scoreLimitButton = document.getElementById("score_limit_button");
-    scoreLimitButton.addEventListener("click",function(){
-    	//prompt("Set new score limit.", 25);
-    	
-    	
-    	
-    	
-    	if(remoteDevice == null){
+    connButton.addEventListener("click",function(){
+    	console.log("Attempting to connect...");
+    	if(remoteDevice === null){
     		adapter.startScan(onDeviceFound);
     	}else{
-    		device.connect(connectSuccess, connectFail);    
+    		remoteDevice.connect(connectSuccess, connectFail);    
     	}	
     });
     
@@ -119,25 +137,33 @@ window.onload = function () {
     }
 
     function connectSuccess() {
-        console.log('Connected to device');
+    	
+        if(serviceUUIDs == null){
+    	console.log(adapter);
+    	console.log('Connected to device');
         console.log(remoteDevice);
-		var serviceUUIDs = remoteDevice.uuids;
+		serviceUUIDs = remoteDevice.uuids;
 		console.log(serviceUUIDs);
-		var gattService = remoteDevice.getService(serviceUUIDs[0]);
+		gattService = remoteDevice.getService(serviceUUIDs[0]);
 		
-		 red_score_characteristic = gattService.characteristics[0];
-		 blue_score_characteristic = gattService.characteristics[1];
+		red_score_characteristic = gattService.characteristics[0];
+		blue_score_characteristic = gattService.characteristics[1];
+		
+		
+		
+		console.log(red_score_characteristic);
+		console.log(blue_score_characteristic);
 		 
-		 console.log(red_score_characteristic);
-		 console.log(blue_score_characteristic);
-		 
-		 red_score_characteristic.readValue(readRedCharacteristicSuccess, readFail);
-		 blue_score_characteristic.readValue(readBlueCharacteristicSuccess);
-		 
-        watchId = remoteDevice.addConnectStateChangeListener(connectionListener, readFail);
+		red_score_characteristic.readValue(readRedCharacteristicSuccess, readFail);
+		setTimeout(function(){ 				
+			blue_score_characteristic.readValue(readBlueCharacteristicSuccess, readFail);
+		}, 500);
+        }
+		
+		     
     }
     
-    var remoteDevice = null;
+  
 
     function onDeviceFound(device) {
         if (remoteDevice === null) {
@@ -152,64 +178,56 @@ window.onload = function () {
   
     function readRedCharacteristicSuccess(value) {
       redScore = value[0];
-    	
-     
+      render();
     }
     
-
     function readBlueCharacteristicSuccess(value) {
-       blueScore = value[0];
-    	
+       blueScore = value[0];	
+       render();
     }
 
     function readFail(error) {
         console.log('readValue() failed: ' + error);
     }
     
- 
-    
-    function writeSuccess(value) {
-        console.log('Written');
+    function writeSuccess() {
+        console.log('written');
     }
 
     function writeFail(error) {
         console.log('writeValue() failed: ' + error);
     }
     
-    var newValue;
-
-    
-    function unloadPage()
-    {
-     if(adapter != null){
-    	 adapter.stopScan();
-     }
-     if(remoteDevice != null){
-    	 	remoteDevice.removeConnectStateChangeListener(watchId);
-	        remoteDevice.disconnect();
-	        remoteDevice = null;
-	    }
-     }
-     
+    function render(){
+    	redScoreText.innerHTML = ""  + redScore;
+    	blueScoreText.innerHTML = ""  + blueScore;
     }
     
-    window.onunload = unloadPage;
-    
-    var connectionListener = {
-    	    onconnected: function(device) {
-    	        console.log('Connected to the device: ' + device.name + ' [' + device.address + ']');
-    	    },
-    	    ondisconnected: function(device) {
-    	        console.log('Disconnected from the device ' + device.name + ' [' + device.address + ']');
-    	        remoteDevice.removeConnectStateChangeListener(watchId);
-    	        remoteDevice.disconnect();
-    	        remoteDevice = null;
-    	    }
-    	};
-   
-    
-    //adapter.startScan(successcallback);
+  //Handle page visibility change events
+    function handleVisibilityChange() {
+      if (document.visibilityState === "hidden") {
+        remoteDevice.disconnect();
+      } else {
+    	  remoteDevice.connect(connectSuccess, connectFail);
+      }
+    }
+
+    document.addEventListener('visibilitychange', handleVisibilityChange, false);
+
 };
+
+window.onerror = function() {
+    console.log("unload function called.") ;
+	if(adapter !== null){
+    	 adapter.stopScan();
+     }
+     if(remoteDevice !== null){
+    	console.log("remoteDevice cleanup..."); 
+	    remoteDevice.disconnect();
+	    remoteDevice = null;
+	 }
+};
+
 
 
 
